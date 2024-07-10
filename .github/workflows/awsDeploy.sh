4f8320ef87326a4a8f3d8a2e947745c8f5ba5457
William Dutton <william.dutton@qld.gov.au>
2024-07-10 14:42:43 +1000

    QOLDEV-896 invalidate from github actions script call

--- .github/workflows/awsDeploy.sh
+++ .github/workflows/awsDeploy.sh
@@ -53,8 +53,5 @@ cd "$WORK_DIR" || exit 1
 aws s3 sync $aws_dry_run --sse AES256 --delete "${REPO_ROOT}/dist" "${S3BUCKET}" --exclude '*.svg'
 aws s3 sync $aws_dry_run --sse AES256 --delete "${REPO_ROOT}/dist" "${S3BUCKET}" --exclude '*' --include '*.svg' --content-type 'image/svg+xml'
 
-if [[ ! -z "$DOMAIN" ]]; then
-  ./awsCloudfrontInvalidateCache.sh $DOMAIN
-fi
 
 echo "Deployment $VERSION completed" >> $GITHUB_STEP_SUMMARY
